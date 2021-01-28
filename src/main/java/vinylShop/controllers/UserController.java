b5a613d3d63eabd2e0e4161034f6d512e8bf73a4
kamil289 <kamilmusial1988@gmail.com>
2021-01-28 18:36:00 +0100

    login

--- src/main/java/vinylShop/controllers/UserController.java
+++ src/main/java/vinylShop/controllers/UserController.java
@@ -1,15 +1,21 @@
 package vinylShop.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import vinylShop.dataBase.IUserRepository;
 import vinylShop.model.User;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    IUserRepository userRepository;
+    
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String loginFrom(Model model){
@@ -20,10 +26,16 @@ public class UserController {
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String authentication(@ModelAttribute User user){
 
-        System.out.println(user.getLogin());
-        System.out.println(user.getPass());
+        userRepository.authentication(user);
 
-        return "main";
+        boolean autResult = userRepository.authentication(user);
+
+        if(autResult){
+            return "redirect:/main";
+        }else{
+            return "redirect:/login";
+        }
+
 
     }
 
