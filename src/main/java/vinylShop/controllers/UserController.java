04ce0f8c86096ee04fd7f897a99c974a35f5f9af
kamil289 <kamilmusial1988@gmail.com>
2021-01-29 18:04:14 +0100

    edycja user

--- src/main/java/vinylShop/controllers/UserController.java
+++ src/main/java/vinylShop/controllers/UserController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import vinylShop.dataBase.IUserRepository;
 import vinylShop.model.User;
+import vinylShop.model.view.ChangePassData;
 import vinylShop.session.SessionObject;
 
 import javax.annotation.Resource;
+
 
 @Controller
 public class UserController {
@@ -24,37 +26,78 @@ public class UserController {
 
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginFrom(Model model){
+    public String loginFrom(Model model) {
         model.addAttribute("userModel", new User());
         return "login";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String authentication(@ModelAttribute User user){
+    public String authentication(@ModelAttribute User user) {
 
         userRepository.authentication(user);
 
-       this.sessionObject.setUser(this.userRepository.authentication(user));
+        this.sessionObject.setUser(this.userRepository.authentication(user));
 
-        if(this.sessionObject.getUser() != null) {
+        if (this.sessionObject.getUser() != null) {
             return "redirect:/main";
-        }else{
+        } else {
             return "redirect:/login";
         }
 
 
     }
+
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
-    public String logout(){
+    public String logout() {
         this.sessionObject.setUser(null);
         return "redirect:/login";
     }
+
     @RequestMapping(value = "/edit", method = RequestMethod.GET)
-    public String edit(Model model){
-        model.addAttribute("user", this.sessionObject.getUser());
+    public String edit(Model model) {
+        if (this.sessionObject.isLogged()) {
+            model.addAttribute("user", this.sessionObject.getUser());
+            model.addAttribute("passModel", new ChangePassData());
+            return "edit";
+        } else {
+            return "redirect:/login";
+        }
+    }
 
-        return "edit";
+    @RequestMapping(value = "/changeData", method = RequestMethod.POST)
+    public String changeData(@ModelAttribute User user) {
+        user.setLogin(this.sessionObject.getUser().getLogin());
+        User updateUser = this.userRepository.updateUserData(user);
+        this.sessionObject.setUser(updateUser);
 
+        return "redirect:/edit";
+    }
+
+    @RequestMapping(value = "/chandePass", method = RequestMethod.POST)
+    public String changePass (@ModelAttribute ChangePassData changePassData) {
+
+        if(!changePassData.getNewPass().equals(changePassData.getRepeatedNewPass())){
+
+            //TODO
+
+        }
+        User user = new User();
+        user.setPass(changePassData.getPass());
+        user.setLogin(this.sessionObject.getUser().getLogin());
+
+        User authenticatedUser = this.userRepository.authentication(user);
+
+        if(authenticatedUser == null){
+
+            //TODO
+        }
+
+        user.setPass(changePassData.getNewPass());
+        User updateUser = this.userRepository.updateUserPass(user);
+        this.sessionObject.setUser(updateUser);
+
+
+        return "redirect:/edit";
     }
 
 }
--- src/main/java/vinylShop/dataBase/IUserRepository.java
+++ src/main/java/vinylShop/dataBase/IUserRepository.java
@@ -2,6 +2,9 @@ package vinylShop.dataBase;
 
 import vinylShop.model.User;
 
+
 public interface IUserRepository {
     User authentication(User user);
+    User updateUserData (User user);
+    User updateUserPass (User user);
 }
